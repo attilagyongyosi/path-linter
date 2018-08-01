8b1c500231e32228e42fcfc4a39e00a7b935470c
attilagyongyosi <attilagyongyosi0@gmail.com>
2018-08-02 00:43:17 +0200

    Utilizing Promises in integration tests.

--- src/cli/__tests__/cli.ispec.ts
+++ src/cli/__tests__/cli.ispec.ts
@@ -1,50 +1,58 @@
 import { fork } from 'child_process';
 import * as path from 'path';
 
-const run = (args: string[], callback: Function) => {
-    const cli = fork(path.join('lib', 'main'), args, { silent: true });
+interface RunOutput {
+    code: number;
+    stdout: string;
+    stderr: string;
+}
 
-    let stdoutBuffer = '';
-    let stderrBuffer = '';
+function run(args: string[]): Promise<RunOutput> {
+    return new Promise((resolve) => {
+        const cli = fork(path.join('lib', 'main'), args, { silent: true });
 
-    cli.stdout.on('data', (data) => { stdoutBuffer += data; });
-    cli.stderr.on('data', (data) => { stderrBuffer += data; });
-    cli.on('close', (code) => { callback(code, stdoutBuffer, stderrBuffer); });
-};
+        let stdout = '';
+        let stderr = '';
+
+        cli.stdout.on('data', (data) => { stdout += data; });
+        cli.stderr.on('data', (data) => { stderr += data; });
+        cli.on('close', (code) => resolve({ code, stdout, stderr }));
+    });
+}
 
 describe('CLI', () => {
     it('should show error when no configuration file was given', (done) => {
-        run([], (code: number, stdout: string, stderr: string) => {
-            expect(code).toBe(1);
-            expect(stderr).toContain('No configuration file given!');
-            expect(stdout).toBe('');
+        run([]).then(output => {
+            expect(output.code).toBe(1);
+            expect(output.stderr).toContain('No configuration file given!');
+            expect(output.stdout).toBe('');
             done();
         });
     });
 
     it('should halt when configuration file can not be read', (done) => {
-        run([ 'non-existing.json' ], (code: number, stdout: string, stderr: string) => {
-            expect(code).toBe(1);
-            expect(stderr).not.toBeFalsy();
-            expect(stdout).toBe('');
+        run([ 'non-existing.json' ]).then(output => {
+            expect(output.code).toBe(1);
+            expect(output.stderr).not.toBeFalsy();
+            expect(output.stdout).toBe('');
             done();
         });
     });
 
     it('should lint kebab-correct folder', (done) => {
-        run([ 'test/kebab-config.json' ], (code: number, stdout: string, stderr: string) => {
-            expect(code).toBe(0);
-            expect(stderr).toBe('');
-            expect(stdout).toContain('Linted 4 files.');
+        run([ 'test/kebab-config.json' ]).then(output => {
+            expect(output.code).toBe(0);
+            expect(output.stderr).toBe('');
+            expect(output.stdout).toContain('Linted 4 files.');
             done();
         });
     });
 
     it('should fail with 1 linting error in should-fail folder', (done) => {
-        run([ 'test/should-fail-config.json' ], (code: number, stdout: string, stderr: string) => {
-            expect(code).toBe(1);
-            expect(stdout).toContain('Linted 3 files.');
-            expect(stderr).toContain('Failed to lint');
+        run([ 'test/should-fail-config.json' ]).then(output => {
+            expect(output.code).toBe(1);
+            expect(output.stdout).toContain('Linted 3 files.');
+            expect(output.stderr).toContain('Failed to lint');
             done();
         });
     });
