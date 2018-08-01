14cb7479ca6b6ea28c836c7fd7d12105276f5f2d
attilagyongyosi <attilagyongyosi0@gmail.com>
2018-08-01 20:38:58 +0200

    Dedicated script for integration testing.

--- src/__tests__/index.integration.spec.ts
+++ src/__tests__/index.integration.spec.ts
@@ -5,18 +5,11 @@ const run = (args: string[], callback: Function) => {
     const cli = fork(path.join('lib', 'lib'), args, { silent: true });
 
     let stdoutBuffer = '';
-    cli.stdout.on('data', (data) => {
-        stdoutBuffer += data;
-    });
-
     let stderrBuffer = '';
-    cli.stderr.on('data', (data) => {
-        stderrBuffer += data;
-    });
 
-    cli.on('close', (code) => {
-        callback(code, stdoutBuffer, stderrBuffer);
-    });
+    cli.stdout.on('data', (data) => { stdoutBuffer += data; });
+    cli.stderr.on('data', (data) => { stderrBuffer += data; });
+    cli.on('close', (code) => { callback(code, stdoutBuffer, stderrBuffer); });
 };
 
 describe('CLI', () => {
@@ -24,6 +17,15 @@ describe('CLI', () => {
         run([], (code: number, stdout: string, stderr: string) => {
             expect(code).toBe(1);
             expect(stderr).toContain('No configuration file given!');
+            expect(stdout).toBe('');
+            done();
+        });
+    });
+
+    it('should halt when configuration file can not be read', (done) => {
+        run([ 'non-existing.json' ], (code: number, stdout: string, stderr: string) => {
+            expect(code).toBe(1);
+            expect(stderr).not.toBeFalsy();
             expect(stdout).toBe('');
             done();
         });
