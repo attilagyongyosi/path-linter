3f4c323bf80fd5684d25d57725e5a86d6e5e0ddf
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-08-26 21:57:59 +0100

    chore: Trimming down Jest configs.

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,13 @@
 module.exports = {
     rootDir: ".",
-    moduleFileExtensions: [ "ts", "js" ],
     moduleDirectories: [
         "node_modules",
         "lib"
     ],
-    globals: {
-        "ts-jest": {
-            tsConfig: "./tsconfig.json"
-        }
-    },
-    testMatch: [
-        "**/__tests__/**/*.spec.ts",
-        "**/+(*.)+spec.+ts"
-    ],
     transform: {
         "^.+\\.ts$": "ts-jest"
     },
-    transformIgnorePatterns: [ "node_modules/(?!@ngrx)" ],
     collectCoverage: true,
     coverageDirectory: "./generated/jest-coverage",
-    coverageReporters: [ "html" ]
+    coverageReporters: [ "html", "text-summary" ]
 };
--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,26 +1,14 @@
 module.exports = {
-    "moduleFileExtensions": [
-        "ts",
-        "js"
-    ],
-    "moduleDirectories": [
+    moduleDirectories: [
         "node_modules",
         "lib"
     ],
-    "globals": {
-        "ts-jest": {
-            tsConfig: "./tsconfig.json"
-        }
-    },
-    "testMatch": [
+    testMatch: [
         "**/__tests__/**/*.ispec.ts",
         "**/+(*.)+ispec.+ts"
     ],
-    "transform": { "^.+\\.ts$": "ts-jest" },
-    "transformIgnorePatterns": [
-        "node_modules/(?!@ngrx)"
-    ],
-    "collectCoverage": true,
-    "coverageDirectory": "./generated/jest-coverage",
-    "coverageReporters": [ "html" ]
-}
+    transform: { "^.+\\.ts$": "ts-jest" },
+    collectCoverage: true,
+    coverageDirectory: "./generated/jest-coverage",
+    coverageReporters: [ "html" ]
+};
