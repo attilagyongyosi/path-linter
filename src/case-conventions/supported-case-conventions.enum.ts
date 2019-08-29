f269a04b14697d25f0d547ae63039f9cfe624c6c
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-08-29 20:05:56 +0100

    feature: Introducing enumeration to specify out-of-the-box supported naming conventions.

---
+++ src/case-conventions/supported-case-conventions.enum.ts
@@ -0,0 +1,12 @@
+/**
+ * Enumeration that lists the naming conventions
+ * that path-linter supports out-of-the-box
+ * without the need to have it configured
+ * with a regular expression by the user.
+ *
+ * @author  Attila_Gyongyosi
+ * @since   1.3.0
+ */
+export enum SupportedCaseConventions {
+    KEBAB_CASE
+}
