49d02b9d7da54b2a5c372fd315bd9188831ef15c
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-09-27 19:55:18 +0100

    chore: removing unused enum fields

--- src/config/validator/config-validator-errors.ts
+++ src/config/validator/config-validator-errors.ts
@@ -1,6 +1,4 @@
 export enum ValidatorErrors {
     NO_RULES = 'Rules array missing in configuration!',
-    NO_DIRECTORY = 'Directory property missing for rule!',
-    NO_REGEXP_OR_CONVENTIONS = 'Either regExp or caseConvention should be specified for a rule!',
-    REGEXP_AND_CONVENTIONS_PRESENT = 'Only specify either regExp or caseConvention!'
+    NO_DIRECTORY = 'Directory property missing for rule!'
 }
