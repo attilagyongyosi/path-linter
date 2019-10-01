a000e521d4c31f27b0bf9fdfee129006648f1374
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-01 19:03:20 +0100

    feat(built-in-kebab): requiring extension at the end of a filename, more tests

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
+const ERROR = 2;
+const MESSAGE_MAX_LENGTH = 140;
+
 module.exports = {
-    extends: [ '@commitlint/config-conventional' ]
+    extends: [ '@commitlint/config-conventional' ],
+    rules: {
+        'header-max-length': [ ERROR, 'always', MESSAGE_MAX_LENGTH ]
+    }
 };
--- src/case-conventions/__tests__/kebab-case.spec.ts
+++ src/case-conventions/__tests__/kebab-case.spec.ts
@@ -2,24 +2,26 @@ import { CONVENTION_MAP } from '../supported-case-conventions.enum';
 
 const KEBAB_CASE_REGEXP = CONVENTION_MAP['kebab-case'];
 
-function pass(path: string): void {
-    expect(KEBAB_CASE_REGEXP.test(path)).toBe(true);
-}
-
-function fail(path: string): void {
-    expect(KEBAB_CASE_REGEXP.test(path)).toBe(false);
-}
+const test = (path: string): boolean => KEBAB_CASE_REGEXP.test(path);
+const pass = (path: string): void => { expect(test(path)).toBe(true); };
+const fail = (path: string): void => { expect(test(path)).toBe(false); };
 
 describe('Built-in kebab-case regexp', () => {
     it('should match valid kebab-case paths', () => {
         pass('some-file.js');
         pass('/your-project-path/some-file.js');
         pass('/your-project-path/some-file12.js');
+        pass('path\\with\\other\\separator-service.ts');
+        pass('my/project/file.xhtml');
+        pass('whatever.service.ts');
     });
 
     it('should fail on non-kebab-compliant paths', () => {
         fail('camelCase.ts');
         fail('snake_path/snake_file.jpg');
         fail('with/@special-chars.wav');
+        fail('__tests__/wat.ts');
+        fail('.gitignore');
+        fail('uPpercase-Path.CSS');
     });
 });
