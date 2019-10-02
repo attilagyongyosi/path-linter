683b54928d1f74dcd6c52590caebe29dfc37200a
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-02 20:50:22 +0100

    chore: using the async-await syntax in integration tests

--- src/cli/__tests__/cli.ispec.ts
+++ src/cli/__tests__/cli.ispec.ts
@@ -1,30 +1,27 @@
 import { run } from '../../../test/utils/cli-runner';
 
 describe('CLI', () => {
-    it('should halt when configuration file can not be read', done => {
-        run([ '--config', 'non-existing.json' ]).then(output => {
-            expect(output.code).toBe(1);
-            expect(output.stderr).toContain('Failed to read configuration');
-            expect(output.stdout).toBe('');
-            done();
-        });
+    it('should halt when configuration file can not be read', async done => {
+        const result = await run([ '--config', 'non-existing.json' ]);
+        expect(result.code).toBe(1);
+        expect(result.stderr).toContain('Failed to read configuration');
+        expect(result.stdout).toBe('');
+        done();
     });
 
-    it('should lint kebab-correct folder', done => {
-        run([ '--config', 'test/kebab-config.json' ]).then(output => {
-            expect(output.code).toBe(0);
-            expect(output.stderr).toBe('');
-            expect(output.stdout).toContain('Linted 4 file(s)');
-            done();
-        });
+    it('should lint kebab-correct folder', async done => {
+        const result = await run([ '--config', 'test/kebab-config.json' ]);
+        expect(result.code).toBe(0);
+        expect(result.stderr).toBe('');
+        expect(result.stdout).toContain('Linted 4 file(s)');
+        done();
     });
 
-    it('should fail with 1 linting error in should-fail folder', done => {
-        run([ '--config', 'test/should-fail-config.json' ]).then(output => {
-            expect(output.code).toBe(1);
-            expect(output.stdout).toContain('Linted 3 file(s)');
-            expect(output.stderr).toContain('does not match');
-            done();
-        });
+    it('should fail with 1 linting error in should-fail folder', async done => {
+        const result = await run([ '--config', 'test/should-fail-config.json' ]);
+        expect(result.code).toBe(1);
+        expect(result.stdout).toContain('Linted 3 file(s)');
+        expect(result.stderr).toContain('does not match');
+        done();
     });
 });
