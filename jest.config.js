e2701a4f0890de0cdbd2baf279e831cc12721b4c
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-05 13:52:30 +0100

    feat(severity): excluding ispec files from unit test coverage

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
             statements: COVERAGE_THRESHOLD
         }
     },
-    collectCoverageFrom: [ 'src/**/*.ts', '!**/cli/**/*.*', '!src/index.ts' ],
+    collectCoverageFrom: [ 'src/**/*.ts', '!**/cli/**/*.*', '!**/*.ispec.ts', '!src/index.ts' ],
     coverageDirectory: './generated/jest-coverage/unit',
     coverageReporters: [ 'html', 'text-summary' ]
 };
