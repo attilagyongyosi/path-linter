f52b0ccee7d64ce3b3a726f5375471b6cb73e3a7
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-05 10:44:37 +0100

    feat(lint-improvement): initial linting algorithm change

--- src/linter/__tests__/linter.spec.ts
+++ src/linter/__tests__/linter.spec.ts
@@ -1,8 +1,21 @@
+import * as path from 'path';
 import { Linter } from '../linter';
 
 describe('Linter', () => {
     it('should lint a file name with a given regular expression', () => {
-        const linter = new Linter(/.*\.js/);
+        const linter = new Linter(/.*/);
         expect(linter.lint('my/path/source.js')).toBe(true);
     });
+
+    it('should lint each part of the path separately', () => {
+        const expectedCallTime: number = 3;
+        const linter = new Linter(/.*/);
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const spy = spyOn(linter as any, 'lintPart').and.callThrough();
+
+        linter.lint(path.join('my', 'path', 'file.ts'));
+
+        expect(spy).toBeCalledTimes(expectedCallTime);
+    })
 });
--- src/linter/linter.ts
+++ src/linter/linter.ts
@@ -1,7 +1,23 @@
-export class Linter {
-    constructor(private rule: RegExp) {}
+import * as path from 'path';
 
-    public lint(path: string): boolean {
-        return this.rule.test(path);
+export class Linter {
+    constructor(private readonly rule: RegExp) {}
+
+    public lint(filePath: string): boolean {
+        if (!filePath) { return false; }
+
+        const pathParts = filePath.split(path.sep);
+
+        for (const part in pathParts) {
+            if (!this.lintPart(part)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private lintPart(filePathPart: string): boolean {
+        return this.rule.test(filePathPart);
     }
 }
