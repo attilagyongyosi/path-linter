50cf923aedc1177c999c59271245411f92e3b029
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-05 13:33:22 +0100

    feat(severity): extending Logger with warning level

--- src/logger/__tests__/logger.spec.ts
+++ src/logger/__tests__/logger.spec.ts
@@ -1,31 +1,36 @@
 import { Logger } from '../logger';
-import { green } from '../../util/color-codes';
+import { green, yellow } from '../../util/color-codes';
 
 describe('CLI Logger', () => {
     const LOG = new Logger();
 
     it('should display an info message', () => {
-        spyOn(console, 'log').and.callThrough();
+        spyOn(console, 'info').and.callThrough();
         LOG.info('Message');
+        expect(console.info).toHaveBeenNthCalledWith(1, '[info] Message');
+    });
 
-        expect(console.log).toHaveBeenCalledTimes(1);
-        expect(console.log).toHaveBeenCalledWith('[info] Message');
+    it('should display a warning message', () => {
+        spyOn(console, 'warn').and.callThrough();
+        LOG.warning('Message');
+        expect(console.warn).toHaveBeenNthCalledWith(1, '[warning] Message');
     });
 
     it('should display an error message', () => {
         spyOn(console, 'error').and.callThrough();
         LOG.error('Message');
-
-        expect(console.error).toHaveBeenCalledTimes(1);
-        expect(console.error).toHaveBeenCalledWith('[error] Message');
+        expect(console.error).toHaveBeenNthCalledWith(1, '[error] Message');
     });
 
     it('should display colorized message', () => {
         const COLOR_LOG = new Logger({ colorize: true, configFile: '' });
-        spyOn(console, 'log').and.callThrough();
+        spyOn(console, 'info').and.callThrough();
+        spyOn(console, 'warn').and.callThrough();
 
         COLOR_LOG.info('Message');
-        expect(console.log).toHaveBeenCalledTimes(1);
-        expect(console.log).toHaveBeenCalledWith(`[${green('info')}] Message`);
+        expect(console.info).toHaveBeenNthCalledWith(1, `[${green('info')}] Message`);
+
+        COLOR_LOG.warning('Message');
+        expect(console.warn).toHaveBeenNthCalledWith(1, `[${yellow('warning')}] Message`);
     });
 });
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,5 +1,5 @@
 import { CliOptions } from '../cli/options/cli-options';
-import { deColorize, green, red } from '../util/color-codes';
+import { deColorize, green, red, yellow } from '../util/color-codes';
 
 /**
  * Provides advanced console logging capabilities
@@ -12,12 +12,17 @@ import { deColorize, green, red } from '../util/color-codes';
  */
 export class Logger {
     private readonly infoPrefix = `[${green('info')}] `;
+    private readonly warningPrefix = `[${yellow('warning')}] `;
     private readonly errorPrefix = `[${red('error')}] `;
 
     constructor(public options: CliOptions = new CliOptions()) {}
 
     public info(message: string): void {
         this.log('info', `${this.infoPrefix}${message}`);
+    }
+
+    public warning(message: string): void {
+        this.log('warn', `${this.warningPrefix}${message}`);
     }
 
     public error(message: string): void {
@@ -27,10 +32,16 @@ export class Logger {
     private log(level: string, message: string): void {
         const newMessage = this.options.colorize ? message : deColorize(message);
 
-        if (level === 'info') {
-            console.log(newMessage);
-        } else {
+        switch (level) {
+        case 'info':
+            console.info(newMessage);
+            break;
+        case 'warn':
+            console.warn(newMessage);
+            break;
+        case 'error':
             console.error(newMessage);
+            break;
         }
     }
 
