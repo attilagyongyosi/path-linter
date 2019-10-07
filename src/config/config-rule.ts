f1d4d7d34bc0cafcb6a44f5809695566604ce236
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-07 22:58:44 +0100

    feat(advanced-linting): documenting ConfigRule and expanding it with the optional ignore property

--- src/config/config-rule.ts
+++ src/config/config-rule.ts
@@ -1,4 +1,27 @@
+/**
+ * @author  attilagyongyosi
+ */
 export interface ConfigRule {
+
+    /**
+     * The directory relative to the project root within
+     * which this rule should be enforced.
+     */
     directory: string;
+
+    /**
+     * Rule is either a used-defines regular expression
+     * or one of the built-in supported conventions.
+     * `kebab-case` is the one currently supported.
+     */
     rule: string;
+
+    /**
+     * Potential list of strings to ignore
+     * when linting paths.
+     *
+     * These will be stripped from file paths
+     * before linting.
+     */
+    ignore?: string[];
 }
