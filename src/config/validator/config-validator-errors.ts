985f32cae206f00e5612063e5c0eb0a9847849ad
attilagyongyosi <attilagyongyosi0@gmail.com>
2019-10-19 12:56:01 +0100

    feat(advanced-linting): fixing typo in, and documenting ValidationErrors

--- src/config/validator/config-validator-errors.ts
+++ src/config/validator/config-validator-errors.ts
@@ -1,7 +1,13 @@
+/**
+ * Enumeration of error messages that the
+ * config validation process could throw.
+ *
+ * @author  attilagyongyosi
+ */
 export enum ValidatorErrors {
     NO_RULES = 'Rules array missing in configuration!',
     NO_DIRECTORY = 'Directory property missing for rule!',
     NO_RULE = 'Missing linting rule for a directory in config!',
-    WRONG_SEVERITY = 'The severity configuration can either be "error" or "warnign"!',
+    WRONG_SEVERITY = 'The severity configuration can either be "error" or "warning"!',
     IGNORE_IS_NOT_ARRAY = 'The ignore property is not an array of strings!'
 }
