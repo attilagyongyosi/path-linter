27ba045c1323491521a831dfb2aa0865908c904a
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-01-17 19:11:49 +0000

    chore: implementing unit tests for path.utils

--- src/util/__tests__/path.utils.spec.ts
+++ src/util/__tests__/path.utils.spec.ts
@@ -1,22 +1,33 @@
+import * as path from 'path';
 import { convertPathSeparators } from '../path.utils';
-import { isWindows } from '../platform.utils';
+import { mockUnixPlatform, mockWindowsPlatform, setMockPathSeparator } from '../../../test/utils/platform-mocking';
 
 describe('Path Utils', () => {
+    const ORIGINAL_PATH_SEPARATOR = path.sep;
+
+    afterEach(() => {
+        setMockPathSeparator(ORIGINAL_PATH_SEPARATOR);
+    });
+
     describe('convertToPathSeparators()', () => {
-        it('should detect and convert path separator characters according to platform', () => {
-            if (isWindows()) {
-                expect(convertPathSeparators('/test/path/file.txt')).toBe('\\test\\path\\file.txt');
-            } else {
-                expect(convertPathSeparators('\\test\\path\\file.txt')).toBe('/test/path/file.txt');
-            }
+        it('should detect and convert path separator characters on Windows', () => {
+            mockWindowsPlatform();
+            expect(convertPathSeparators('/test/path/file.txt')).toBe('\\test\\path\\file.txt');
         });
 
-        it('should leave current platform separators unchanged', () => {
-            if (isWindows()) {
-                expect(convertPathSeparators('C:\\some\\windows\\path')).toBe('C:\\some\\windows\\path');
-            } else {
-                expect(convertPathSeparators('/some/windows/path')).toBe('/some/windows/path');
-            }
+        it('should detect and convert path separator characters on Unix', () => {
+            mockUnixPlatform();
+            expect(convertPathSeparators('\\test\\path\\file.txt')).toBe('/test/path/file.txt');
+        });
+
+        it('should leave current platform separators unchanged on Windows', () => {
+            mockWindowsPlatform();
+            expect(convertPathSeparators('C:\\some\\windows\\path')).toBe('C:\\some\\windows\\path');
+        });
+
+        it('should leave current platform separators unchanged on Unix', () => {
+            mockUnixPlatform();
+            expect(convertPathSeparators('/some/windows/path')).toBe('/some/windows/path');
         });
     });
 });
