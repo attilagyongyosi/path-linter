6a2e66a37154988d9e6630bd2b4942fb6da1dc41
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-01-18 14:12:10 +0000

    chore: documenting code

--- src/util/path.utils.ts
+++ src/util/path.utils.ts
@@ -4,6 +4,27 @@ import { isWindows } from './platform.utils';
 const WINDOWS_PATH_SEPARATOR = /\\/;
 const UNIX_PATH_SEPARATOR = /\//;
 
+/**
+ * Given a path-like string (e.g.: src/path/file.ext), this function
+ * will convert the detected path-separator characters to the ones
+ * supported by the execution environment platform.
+ *
+ * Supports Unix and Windows platforms.
+ *
+ * Implementation relies on NodeJS' 'os' module to determine
+ * the current platform.
+ *
+ * Other characters should be left unchanged by this.
+ *
+ * @param   pathToConvert
+ *          a path-like string to convert path separators in.
+ *
+ * @returns with a new string that has the path separators
+ *          converted to that of the current platform.
+ *          e.g.: unix/path/file.ext -> unix\\path\\file.ext
+ *
+ * @author  attilagyongyosi
+ */
 export function convertPathSeparators(pathToConvert: string): string {
     let pathSeparatorRegexp: RegExp = new RegExp(WINDOWS_PATH_SEPARATOR, 'g');
 
