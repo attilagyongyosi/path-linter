bc15d9fcc4969d86a9c09b95ca456ce85d7a1ee3
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-01-18 13:13:30 +0000

    fix: path separators in utility code as regular exp rather than strings

--- src/util/path.utils.ts
+++ src/util/path.utils.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
 import { isWindows } from './platform.utils';
 
-const WINDOWS_PATH_SEPARATOR = '\\\\';
-const UNIX_PATH_SEPARATOR = '/';
+const WINDOWS_PATH_SEPARATOR = /\\/;
+const UNIX_PATH_SEPARATOR = /\//;
 
 export function convertPathSeparators(pathToConvert: string): string {
     let pathSeparatorRegexp: RegExp = new RegExp(WINDOWS_PATH_SEPARATOR, 'g');
