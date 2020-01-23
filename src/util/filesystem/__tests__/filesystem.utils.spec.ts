6c69567b906b444b53a27807f47c1e0cbb5bfcec
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-01-23 23:34:17 +0000

    feat: extending FilesystemUtils with a function to find files, documenting, testing

--- src/util/filesystem/__tests__/filesystem.utils.spec.ts
+++ src/util/filesystem/__tests__/filesystem.utils.spec.ts
@@ -1,27 +1,53 @@
-import { fileExists } from '../filesystem.utils';
 import * as path from 'path';
+import utils from '../filesystem.utils';
 
 const context = (fileName: string): string => path.join(__dirname, fileName);
 
 describe('Filesystem Utils', () => {
     describe('fileExists()', () => {
         it('should return true when path exists and is a file', done => {
-            fileExists(context('filesystem.utils.spec.ts')).then(doesExist => {
+            utils.fileExists(context('filesystem.utils.spec.ts')).then(doesExist => {
                 expect(doesExist).toBe(true);
                 done();
             });
         });
 
         it('should return false when path exists but is a directory', done => {
-            fileExists(context('/')).then(doesExist => {
+            utils.fileExists(context('/')).then(doesExist => {
                 expect(doesExist).toBe(false);
                 done();
             });
         });
 
         it('should return false when path does not exist', done => {
-            fileExists(context('phantom.js')).then(doesExist => {
+            utils.fileExists(context('phantom.js')).then(doesExist => {
                 expect(doesExist).toBe(false);
+                done();
+            });
+        });
+    });
+
+    describe('findFile()', () => {
+        it('should find a single file', done => {
+            jest.spyOn(utils, 'fileExists').mockImplementation(() => Promise.resolve(true));
+            utils.findFile('.', 'filesystem.utils.spec.ts').then(foundFile => {
+                expect(foundFile).toBe('filesystem.utils.spec.ts');
+                done();
+            });
+        });
+
+        it('should return first match from multiple findings', done => {
+            jest.spyOn(utils, 'fileExists').mockImplementation(() => Promise.resolve(true));
+            utils.findFile('', 'file1', 'file2', 'file3').then(foundFile => {
+                expect(foundFile).toBe('file1');
+                done();
+            });
+        });
+
+        it('should yield an empty path when no files are found', done => {
+            jest.spyOn(utils, 'fileExists').mockImplementation(() => Promise.resolve(false));
+            utils.findFile('.', 'file1', 'file2').then(result => {
+                expect(result).toBe('');
                 done();
             });
         });
--- src/util/filesystem/filesystem.utils.ts
+++ src/util/filesystem/filesystem.utils.ts
@@ -1,29 +1,71 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
-/**
- * Checks whether the given path string is an
- * existing file in the file system.
- *
- * @param   path
- *          A string representing a path to check.
- *
- * @returns with a Promise that resolves to `true` when
- *          the is a valid file or `false` if it doesn't
- *          exist or is a directory.
- *
- * @author  attilagyongyosi
- */
-export async function fileExists(path: string): Promise<boolean> {
-    return stats(path)
-        .then(result => result.isFile())
-        .catch(() => false);
-}
+export default {
 
-export async function stats(path: string): Promise<fs.Stats> {
-    return new Promise<fs.Stats>((resolve, reject) => {
-        fs.stat(path, (error, result) => {
-            if (error) { reject(error); }
-            resolve(result);
+    /**
+     * Checks whether the given path string is an
+     * existing file in the file system.
+     *
+     * @param   path
+     *          A string representing a path to check.
+     *
+     * @returns with a Promise that resolves to `true` when
+     *          the is a valid file or `false` if it doesn't
+     *          exist or is a directory.
+     *
+     * @author  attilagyongyosi
+     */
+    fileExists: function (path: string): Promise<boolean> {
+        return this.stats(path)
+            .then(result => result.isFile())
+            .catch(() => false);
+    },
+
+    /**
+     * Given a directory and a set of file names,
+     * this function will check if any of the files exist
+     * in the folder and returns with the path of the first
+     * one that matches.
+     *
+     * @param   directory
+     *          The directory path as a string to search in.
+     *
+     * @param   fileNames
+     *          An comma-separated set of file names
+     *          to search for.
+     *
+     * @returns with the first file of the supplied list
+     *          that can be found in the folder.
+     *
+     * @author  attilagyongyosi
+     */
+    findFile: function (directory: string, ...fileNames: string[]): Promise<string> {
+        const filePaths = fileNames.map(fileName => path.join(directory, fileName));
+        const existsPromises = filePaths.map(filePath => this.fileExists(filePath));
+
+        return Promise.all(existsPromises).then(results => {
+            return filePaths[results.indexOf(true)] || '';
         });
-    });
-}
+    },
+
+    /**
+     * Makes a Promise from NodeJS' stats() function.
+     *
+     * @param   path
+     *          The path to get the file stats from.
+     *
+     * @returns with a Promise that resolves with a Stats object
+     *          or rejects with an error if there is any.
+     *
+     * @author  attilagyongyosi
+     */
+    stats: function (path: string): Promise<fs.Stats> {
+        return new Promise<fs.Stats>((resolve, reject) => {
+            fs.stat(path, (error, result) => {
+                if (error) { reject(error); }
+                resolve(result);
+            });
+        });
+    }
+};
