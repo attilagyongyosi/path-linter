54e5aea5b8a85fd228a99cdd6cad04f0f6f45fa5
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-01-24 21:36:16 +0000

    docs: document default configuration paths

--- src/config/default-config-paths.ts
+++ src/config/default-config-paths.ts
@@ -1,4 +1,19 @@
-/* istanbul ignore file  // will be checked by tests later */
+/**
+ * The file names in this array
+ * are considered default configuration file names.
+ *
+ * If no --config switch is given to the
+ * path-linter executable, it will try to locate
+ * one of these in the project's directory,
+ * in the order they are specified here.
+ *
+ * If none is found, an error message is displayed.
+ *
+ * @see     ConfigReader
+ *
+ * @since   2.1.0
+ * @author  attilagyongyosi
+ */
 export const DEFAULT_CONFIG_PATHS: string[] = [
     'path-linter.json',
     '.path-linter.json',
