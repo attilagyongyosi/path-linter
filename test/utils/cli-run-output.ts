70b06a815bcec5bf1e09a5f53bbcad9e214fdb6e
attilagyongyosi <attilagyongyosi0@gmail.com>
2021-03-29 22:23:30 +0100

    test: updating RunOutput interface to comply with Node.js typing updates, and documenting code

--- test/utils/cli-run-output.ts
+++ test/utils/cli-run-output.ts
@@ -1,5 +1,14 @@
+/**
+ * Simple interface describing the output of a Node.js process execution.
+ * Used for integration testing based on process spawning.
+ *
+ * Will be deprecated in v2.2.0 as integration testing approach will
+ * be changed to not rely on creating processes.
+ *
+ * @author  attilagyongyosi
+ */
 export interface RunOutput {
-    code: number;
+    code: number | null;
     stdout: string;
     stderr: string;
 }
